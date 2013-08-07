22b8f2bf183525aae26ac0e1bc96362f70abd01d
Arnel Perez <arnelperez@Arnels-Mac-mini-2.local>
2013-08-07 14:29:15 +0800

    Initial Commit

---
+++ XMLFetch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  XMLFetch
+//
+//  Created by Arnel Perez on 8/7/13.
+//  Copyright (c) 2013 247. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ XMLFetch/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  XMLFetch
+//
+//  Created by Arnel Perez on 8/7/13.
+//  Copyright (c) 2013 247. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
